d08fa12a58c82e860fd03a791adb5c4f3628c648
DevAgent <dev@pathfinder.local>
2026-02-26 21:49:14 +0000

    Fix: add image domains to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'thumbs.dreamstime.com' },
+      { protocol: 'https', hostname: 'i0.wp.com' },
+      { protocol: 'https', hostname: 'media.gettyimages.com' },
+      { protocol: 'https', hostname: 'static.vecteezy.com' },
+      { protocol: 'https', hostname: 'www.adventuretreks.com' },
+      { protocol: 'https', hostname: 'www.outdooreducationcenter.org' },
+      { protocol: 'https', hostname: 'tillyslifecenter.org' },
+    ],
   },
 };
 
